e105d3c360a5b117a72a6e785b2b1e9d7b473ce7
millea <millea1994@gmail.com>
2019-03-08 22:06:09 +0700

    add templating system and implement of class based view

--- account/urls.py
+++ account/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
 
-urlpattern = [
-    path('signup/', views.SignUp.as_view(), name=sigup),
+urlpatterns = [
+    path('signup/', views.SignUp.as_view(), name='signup')
 ]
