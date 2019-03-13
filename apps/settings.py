2519674e2473097d239c72d70ac59ca458c02748
millea <millea1994@gmail.com>
2019-03-13 21:43:04 +0700

    Admin dashboard and setting redirect

--- apps/settings.py
+++ apps/settings.py
@@ -121,3 +121,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGOUT_REDIRECT_URL = '/account/login'
+LOGIN_REDIRECT_URL = '/'
--- apps/urls.py
+++ apps/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 
 urlpatterns = [
+	path('', TemplateView.as_view(template_name='welcome.html')),
     path('admin/', admin.site.urls),
     path('account/', include('account.urls')),
     path('account/', include('django.contrib.auth.urls')),
