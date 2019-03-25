1463146183516ccc0ef9abd395e4705143f94ed3
millea <millea1994@gmail.com>
2019-03-25 21:56:57 +0700

    Display data and create list view

--- forums/urls.py
+++ forums/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import ForumCreate
+from .views import ForumCreate, ForumListView
 
 urlpatterns = [
+	path('', ForumListView.as_view(), name='forum-list'),
 	path('add/', ForumCreate.as_view(), name='forum-add')
 ]
--- forums/views.py
+++ forums/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
-from django.views.generic.edit import CreateView
+from django.views.generic import CreateView, ListView
 from .models import Forum
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
 
 @method_decorator(login_required, name='dispatch')
+
+class ForumListView(ListView):
+	model = Forum
+	context_object_name  = "objForums"
+	queryset = Forum.objects.order_by('created_at')
+
+	# queryset = Forum.objects.order_by('created_at','desc')
+
 class ForumCreate(CreateView):
 	model = Forum
 	fields = ['title','desc']
