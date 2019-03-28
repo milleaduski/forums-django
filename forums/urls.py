3f7ea5231f6da4f033986b9478401a8ceb933f86
millea <millea1994@gmail.com>
2019-03-28 23:05:48 +0700

    Single views and routing params

--- forums/urls.py
+++ forums/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import ForumCreate, ForumListView
+from .views import ForumCreate, ForumListView, ForumUserListView
 
 urlpatterns = [
 	path('', ForumListView.as_view(), name='forum-list'),
+	path('by/<username>/', ForumUserListView.as_view(), name='forum-by'),
 	path('add/', ForumCreate.as_view(), name='forum-add')
 ]
--- forums/views.py
+++ forums/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views.generic import CreateView, ListView
 from .models import Forum
+from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
 
@@ -8,10 +9,14 @@ from django.utils.decorators import method_decorator
 
 class ForumListView(ListView):
 	model = Forum
-	context_object_name  = "objForums"
+	#context_object_name  = "objForums"
 	queryset = Forum.objects.order_by('created_at')
 
-	# queryset = Forum.objects.order_by('created_at','desc')
+class ForumUserListView(ListView):
+	template_name = 'forums/forum_by_user.html'
+	def get_queryset(self):
+		self.user = get_object_or_404(User, username = self.kwargs['username'])
+		return Forum.objects.filter(user = self.user)
 
 class ForumCreate(CreateView):
 	model = Forum
