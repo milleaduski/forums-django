268433962dd3e16301c3fffca35d8b290b989087
millea <millea1994@gmail.com>
2019-05-02 22:12:32 +0700

    activation link and decode token

--- account/views.py
+++ account/views.py
@@ -2,13 +2,14 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, PasswordChangeForm
 from django.contrib.auth import update_session_auth_hash
 from django.template.loader import render_to_string
-from django.utils.encoding import force_bytes
+from django.utils.encoding import force_bytes, force_text
 from django.urls import reverse, reverse_lazy
 from django.views import generic
 from django.contrib import messages
 from .forms import SignUpForm
 from .token import account_activation_token
-from django.utils.http import urlsafe_base64_encode
+from django.contrib.auth.models import User
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 from django.contrib.sites.shortcuts import get_current_site
 
 def SignUp(request):
@@ -25,7 +26,7 @@ def SignUp(request):
 			message = render_to_string('activation_email.html',{
 					'user' : user,
 					'domain' : get_current_site(request).domain,
-					'uid' : urlsafe_base64_encode(force_bytes(user.pk)),
+					'uid' : urlsafe_base64_encode(force_bytes(user.pk)).decode(),
 					'token' : token
 				}
 			)
@@ -49,5 +50,12 @@ def ChangePassword(request):
 ## OLD password: newadmin1994
 ## New password: admin1994
 
-def Activate(request):
-	return
+def Activate(request, uidb64, token):
+	uid = force_text(urlsafe_base64_decode(uidb64).decode())
+	user = User.objects.get(pk=uid)
+
+	if user is not None and account_activation_token.check_token(user, token):
+		user.is_active = True
+		user.profile.email_validated = True
+		user.save()
+		return redirect(reverse('home'))
--- forums/models.py
+++ forums/models.py
@@ -3,6 +3,8 @@ from django.conf import settings
 from django.utils.text import slugify
 from django.urls import reverse
 from django.contrib.auth.models import User
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 import time
 
 class Forum(models.Model):
@@ -36,3 +38,10 @@ class Profile(models.Model):
 
 	def __str__(self):
 		return self.user.username
+
+@receiver(post_save, sender=User, dispatch_uid="save_new_user_profile")
+def save_profile(sender, instance, created, **kwargs):
+	user = instance
+	if created:
+		profile = Profile(user=user)
+		profile.save()
